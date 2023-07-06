e7d7ff85f049f42218966570363fa208e9788523
Cenivell <michutka20085@gmail.com>
2023-07-06 14:40:35 +0300

    Part 2

--- trip_calculator.py
+++ trip_calculator.py
@@ -31,13 +31,13 @@
 # Частина 2: Розрахунок продуктів.
 # Відомо, що на день одній людині потрібно ~2200 кіло-калорій, щоб комфортно почуватися.
 # Продукти поході повинні довго зберігатися та бути герметично закритими, бажано без залишків із попреднього дня.
-# Також під час походу важливо зберігати питний режим, тому потрібно вдень вживати ~2 літри води. 
+# Також під час походу важливо зберігати питний режим, тому потрібно вдень вживати ~2 літри води.
 
 # Технічне завдання:
 # Попередні розрахунки зберігаються.
 # Вхідні параметри ті ж самі.
 # Результатом роботи коду має бути:
-# 1) Розрахунок продуктів, потрібних на вказану кількості денів та осіб. 
+# 1) Розрахунок продуктів, потрібних на вказану кількості денів та осіб.
 # 2) Розрахунок ваги продуктів загалом
 # 3) Розрахунок ваги продукті з розподілом на кожного учасника походу.
 # Результатом роботи коду має бути вивід кожного елемента,у форматі dict : key=item, value=calculated,
@@ -70,7 +70,7 @@ duration_days = int(duration_days)
 knifes = num_of_participation/2
 if knifes <= 2: knifes = 2
 #Словник
-trip_dict = {
+trip_items_dict = {
     'backpacks': num_of_participation,
     'raincoats': num_of_participation,
     'sleeping_bags': num_of_participation,
@@ -81,6 +81,29 @@ trip_dict = {
     'bowls': num_of_participation,
     'knifes': round(knifes), #Закругляю змінну
 }
+#Щоб не робити повторювальну дії вивожу в змінну
+multiply_people_by_days=num_of_participation*duration_days
+#Словник для продуктів
+trip_food_dict = {
+    'Гранола горіхова з шоколадом': multiply_people_by_days, #calories - 442 water -200 weight -100
+    'Харчо грузинське з яловичиною': multiply_people_by_days, #calories - 187 water - 350 weight - 57
+    'Гречка з яловичиною': multiply_people_by_days, #calories - 320 water - 350 weight - 100
+    'Чай карпатський "Чаїдло"': multiply_people_by_days, #calories - 213 water - 1000 weight - 40
+    'Чай карпатський "Чаїдло"': multiply_people_by_days, #calories - 458 weight - 100
+    'Батончик енергетичний OMNOM неспішне какао"': multiply_people_by_days * 3,  #calories - 621 weight - 150(3 на день)
+}
+#Список ваги продуктів
+food_weight_list=[100,57,100,40,100,450]
+#Загально вага продуктів
+total_food_weight=multiply_people_by_days*sum(food_weight_list)
+#Вага на людину
+food_weight_per_person=total_food_weight/num_of_participation
 #Вивожу данні в консоль
-for key, value in trip_dict.items():
+print('------------------Items------------------')
+for key, value in trip_items_dict.items():
     print('Amount of', key, value)
+print('----------------Products----------------')
+for key, value in trip_food_dict.items():
+    print('Amount of', key, value)
+print("Total food weight is",total_food_weight)
+print("Food weight per person is",int(food_weight_per_person))
